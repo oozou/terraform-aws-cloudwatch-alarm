3c012d242cf2bd153f9dd1b67fce5244bfe7232a
sedthawut tipkanpirome <sedthawut2000@gmail.com>
2022-08-01 16:12:13 +0700

    (add): simple usage for rds alerting

---
+++ modules/aws-rds-alerting/examples/simple/main.tf
@@ -0,0 +1,26 @@
+module "kafka_alerting" {
+  source = "../../"
+
+  prefix      = var.prefix
+  environment = var.environment
+  name        = var.name
+
+  db_instance_identifier    = "oozou-devops-demo-db"
+  enabled_cloudwatch_alarms = ["cpu_high", "mem_high"]
+  additional_cloudwatch_alarms = {
+    cpu_high_x = {
+      metric_name         = "CPUCreditUsage"
+      namespace           = "AWS/RDS"
+      comparison_operator = ">="
+      threshold           = "10"
+      evaluation_periods  = "1"
+      statistic           = "Maximum"
+      period              = "60"
+      dimensions = {
+        "DBInstanceIdentifier" = "oozou-devops-demo-db"
+      }
+    }
+  }
+
+  tags = var.custom_tags
+}
---
+++ modules/aws-rds-alerting/examples/simple/terraform.example.tfvars
@@ -0,0 +1,6 @@
+prefix      = "oozou"
+environment = "devops"
+name        = "cms-rds"
+custom_tags = {
+  "Remark" = "terraform-aws-rds-alerting-example"
+}
