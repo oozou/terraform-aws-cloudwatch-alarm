2ef03ae5ae852396a633182b5708f38c8c15dd2e
sedthawut tipkanpirome <sedthawut2000@gmail.com>
2022-08-02 18:26:14 +0700

    (update): defualt to alerting

---
+++ modules/aws-doc-db-alerting/variables.tf
@@ -0,0 +1,67 @@
+/* -------------------------------------------------------------------------- */
+/*                                   Generic                                  */
+/* -------------------------------------------------------------------------- */
+variable "prefix" {
+  description = "The prefix name of customer to be displayed in AWS console and resource"
+  type        = string
+}
+
+variable "environment" {
+  description = "Environment Variable used as a prefix"
+  type        = string
+}
+
+variable "name" {
+  description = "Name alarm"
+  type        = string
+}
+
+variable "tags" {
+  description = "Custom tags which can be passed on to the AWS resources. They should be key value pairs having distinct keys"
+  type        = map(any)
+  default     = {}
+}
+
+/* -------------------------------------------------------------------------- */
+/*                             Alarm Configuration                            */
+/* -------------------------------------------------------------------------- */
+variable "db_cluster_identifier" {
+  description = "The name of the db cluster"
+  type        = string
+}
+
+variable "enabled_cloudwatch_alarms" {
+  description = "Set of cloudwatch alarm to alert"
+  type        = list(string)
+  default     = []
+  validation {
+    condition = alltrue([
+      for value in var.enabled_cloudwatch_alarms : contains(
+        [
+          "cpu_high"
+        ],
+        value
+      )
+    ])
+    error_message = "The given value is not valid choice."
+  }
+}
+
+variable "additional_cloudwatch_alarms" {
+  description = "(optional) Additional cloudwatch alarm"
+  type        = any
+  default     = {}
+}
+
+variable "default_alarm_actions" {
+  description = "(optional) Default alarm action for every alarms"
+  type        = list(string)
+  default     = []
+}
+
+/* ------------------------ Set Default Module Alert ------------------------ */
+variable "override_cpu_high_alert" {
+  description = "Key value to override default configuration; possible keys: {threshold, evaluation_periods, statistic, period}"
+  type        = map(any)
+  default     = {}
+}
