095d3b4c1453edc8bad14d255d380ea5d5c08cdb
xshot9011 <40098197+xshot9011@users.noreply.github.com>
2023-01-23 13:38:36 +0700

    feat: add sub module for rds (#4)
    * (add): sub module for rds cpu,mem utilization
    
    * (update): README
    
    * (add): simple usage for rds alerting
    
    * (update): move cpu metric to use enhance monitoring
    
    * Update main.tf

---
+++ modules/aws-rds-alerting/README.md
@@ -0,0 +1,49 @@
+<!-- BEGIN_TF_DOCS -->
+## Requirements
+
+| Name                                                                      | Version  |
+|---------------------------------------------------------------------------|----------|
+| <a name="requirement_terraform"></a> [terraform](#requirement\_terraform) | >= 1.0.0 |
+| <a name="requirement_aws"></a> [aws](#requirement\_aws)                   | >= 4.0.0 |
+
+## Providers
+
+| Name                                              | Version |
+|---------------------------------------------------|---------|
+| <a name="provider_aws"></a> [aws](#provider\_aws) | 4.24.0  |
+
+## Modules
+
+| Name                                                | Source | Version |
+|-----------------------------------------------------|--------|---------|
+| <a name="module_alarm"></a> [alarm](#module\_alarm) | ../../ | n/a     |
+
+## Resources
+
+| Name                                                                                                                                                      | Type        |
+|-----------------------------------------------------------------------------------------------------------------------------------------------------------|-------------|
+| [aws_cloudwatch_log_metric_filter.cpu_usage](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/cloudwatch_log_metric_filter)    | resource    |
+| [aws_cloudwatch_log_metric_filter.free_mem](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/cloudwatch_log_metric_filter)     | resource    |
+| [aws_cloudwatch_log_metric_filter.inactive_mem](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/cloudwatch_log_metric_filter) | resource    |
+| [aws_cloudwatch_log_metric_filter.total_mem](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/cloudwatch_log_metric_filter)    | resource    |
+| [aws_db_instance.this](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/db_instance)                                        | data source |
+
+## Inputs
+
+| Name                                                                                                                       | Description                                                                                                     | Type           | Default | Required |
+|----------------------------------------------------------------------------------------------------------------------------|-----------------------------------------------------------------------------------------------------------------|----------------|---------|:--------:|
+| <a name="input_additional_cloudwatch_alarms"></a> [additional\_cloudwatch\_alarms](#input\_additional\_cloudwatch\_alarms) | (optional) Additional cloudwatch alarm                                                                          | `any`          | `{}`    |    no    |
+| <a name="input_db_instance_identifier"></a> [db\_instance\_identifier](#input\_db\_instance\_identifier)                   | The name of the RDS instance                                                                                    | `string`       | n/a     |   yes    |
+| <a name="input_default_alarm_actions"></a> [default\_alarm\_actions](#input\_default\_alarm\_actions)                      | (optional) Default alarm action for every alarms                                                                | `list(string)` | `[]`    |    no    |
+| <a name="input_enabled_cloudwatch_alarms"></a> [enabled\_cloudwatch\_alarms](#input\_enabled\_cloudwatch\_alarms)          | Set of cloudwatch alarm to alert                                                                                | `list(string)` | `[]`    |    no    |
+| <a name="input_environment"></a> [environment](#input\_environment)                                                        | Environment Variable used as a prefix                                                                           | `string`       | n/a     |   yes    |
+| <a name="input_name"></a> [name](#input\_name)                                                                             | Name alarm                                                                                                      | `string`       | n/a     |   yes    |
+| <a name="input_override_cpu_high_alert"></a> [override\_cpu\_high\_alert](#input\_override\_cpu\_high\_alert)              | Key value to override default configuration; possible keys: {threshold, evaluation\_periods, statistic, period} | `map(any)`     | `{}`    |    no    |
+| <a name="input_override_mem_high_alert"></a> [override\_mem\_high\_alert](#input\_override\_mem\_high\_alert)              | Key value to override default configuration; possible keys: {threshold, evaluation\_periods, statistic, period} | `map(any)`     | `{}`    |    no    |
+| <a name="input_prefix"></a> [prefix](#input\_prefix)                                                                       | The prefix name of customer to be displayed in AWS console and resource                                         | `string`       | n/a     |   yes    |
+| <a name="input_tags"></a> [tags](#input\_tags)                                                                             | Custom tags which can be passed on to the AWS resources. They should be key value pairs having distinct keys    | `map(any)`     | `{}`    |    no    |
+
+## Outputs
+
+No outputs.
+<!-- END_TF_DOCS -->
---
+++ modules/aws-rds-alerting/main.tf
@@ -0,0 +1,195 @@
+locals {
+  name = format("%s-%s-%s", var.prefix, var.environment, var.name)
+
+  comparison_operators = {
+    ">=" = "GreaterThanOrEqualToThreshold",
+    ">"  = "GreaterThanThreshold",
+    "<"  = "LessThanThreshold",
+    "<=" = "LessThanOrEqualToThreshold",
+  }
+
+  tags = merge(
+    {
+      "Environment" = var.environment,
+      "Terraform"   = "true"
+    },
+    var.tags
+  )
+}
+
+locals {
+  aws_rds_namespace    = "AWS/RDS"
+  custom_rds_namespace = "RDSInfo"
+
+  cpu_high_alert = merge(
+    {
+      threshold          = 85
+      evaluation_periods = "1"
+      statistic          = "Maximum"
+      period             = "300"
+    },
+    var.override_cpu_high_alert
+  )
+
+  mem_high_alert = merge(
+    {
+      threshold          = 85
+      evaluation_periods = "1"
+      statistic          = "Maximum"
+      period             = "60"
+    },
+    var.override_mem_high_alert
+  )
+
+  enabled_cloudwatch_alarms = length(var.additional_cloudwatch_alarms) == 0 ? var.enabled_cloudwatch_alarms : concat(var.enabled_cloudwatch_alarms, keys(var.additional_cloudwatch_alarms))
+  cloudwatch_alarms = merge(
+    {
+      cpu_high = {
+        metric_name         = "CPUUtilization"
+        namespace           = local.custom_rds_namespace
+        comparison_operator = ">="
+        threshold           = local.cpu_high_alert.threshold
+        evaluation_periods  = local.cpu_high_alert.evaluation_periods
+        statistic           = local.cpu_high_alert.statistic
+        period              = local.cpu_high_alert.period
+      }
+      mem_high = {
+        comparison_operator = ">="
+        evaluation_periods  = local.mem_high_alert.evaluation_periods
+        threshold           = local.mem_high_alert.threshold
+        alarm_actions       = []
+        metric_query = [
+          {
+            id          = "e1"
+            expression  = "100-((free_mem+inactive_mem)*100/total_mem)"
+            label       = "MemoryUtilization"
+            return_data = "true"
+            metric      = []
+          },
+          {
+            id = "free_mem"
+            metric = [
+              {
+                metric_name = "MemoryFree"
+                namespace   = local.custom_rds_namespace
+                period      = local.mem_high_alert.period
+                stat        = local.mem_high_alert.statistic
+              }
+            ]
+          },
+          {
+            id = "inactive_mem"
+            metric = [
+              {
+                metric_name = "MemoryInactive"
+                namespace   = local.custom_rds_namespace
+                period      = local.mem_high_alert.period
+                stat        = local.mem_high_alert.statistic
+              }
+            ]
+          },
+          {
+            id = "total_mem"
+            metric = [
+              {
+                metric_name = "MemoryTotal"
+                namespace   = "RDSInfo"
+                period      = local.mem_high_alert.period
+                stat        = local.mem_high_alert.statistic
+              }
+            ]
+          }
+        ]
+      }
+    },
+    var.additional_cloudwatch_alarms
+  )
+}
+
+data "aws_db_instance" "this" {
+  db_instance_identifier = var.db_instance_identifier
+}
+
+resource "aws_cloudwatch_log_metric_filter" "cpu_usage" {
+  count = contains(local.enabled_cloudwatch_alarms, "cpu_high") && !contains(keys(var.additional_cloudwatch_alarms), "cpu_high") ? 1 : 0
+
+  name           = format("%s-cpu-utilization", local.name)
+  pattern        = "{$.instanceID = \"${var.db_instance_identifier}\"}"
+  log_group_name = "RDSOSMetrics"
+
+  metric_transformation {
+    name      = "CPUUtilization"
+    namespace = local.custom_rds_namespace
+    value     = "$.cpuUtilization.total"
+    unit      = "Percent"
+  }
+}
+
+resource "aws_cloudwatch_log_metric_filter" "total_mem" {
+  count = contains(local.enabled_cloudwatch_alarms, "mem_high") && !contains(keys(var.additional_cloudwatch_alarms), "mem_high") ? 1 : 0
+
+  name           = format("%s-total-mem", local.name)
+  pattern        = "{$.instanceID = \"${var.db_instance_identifier}\"}"
+  log_group_name = "RDSOSMetrics"
+
+  metric_transformation {
+    name      = "MemoryTotal"
+    namespace = local.custom_rds_namespace
+    value     = "$.memory.total"
+    unit      = "Kilobytes"
+  }
+}
+
+resource "aws_cloudwatch_log_metric_filter" "free_mem" {
+  count = contains(local.enabled_cloudwatch_alarms, "mem_high") && !contains(keys(var.additional_cloudwatch_alarms), "mem_high") ? 1 : 0
+
+  name           = format("%s-free-mem", local.name)
+  pattern        = "{$.instanceID = \"${var.db_instance_identifier}\"}"
+  log_group_name = "RDSOSMetrics"
+
+  metric_transformation {
+    name      = "MemoryFree"
+    namespace = local.custom_rds_namespace
+    value     = "$.memory.free"
+    unit      = "Kilobytes"
+  }
+}
+
+resource "aws_cloudwatch_log_metric_filter" "inactive_mem" {
+  count = contains(local.enabled_cloudwatch_alarms, "mem_high") && !contains(keys(var.additional_cloudwatch_alarms), "mem_high") ? 1 : 0
+
+  name           = format("%s-inactive-mem", local.name)
+  pattern        = "{$.instanceID = \"${var.db_instance_identifier}\"}"
+  log_group_name = "RDSOSMetrics"
+
+  metric_transformation {
+    name      = "MemoryInactive"
+    namespace = local.custom_rds_namespace
+    value     = "$.memory.inactive"
+    unit      = "Kilobytes"
+  }
+}
+
+module "alarm" {
+  source = "../../"
+
+  for_each = toset(local.enabled_cloudwatch_alarms)
+
+  prefix      = var.prefix
+  environment = var.environment
+  name        = format("%s-%s", var.name, replace(each.key, "_", "-"))
+
+  comparison_operator = local.comparison_operators[lookup(local.cloudwatch_alarms[each.key], "comparison_operator", null)]
+  evaluation_periods  = lookup(local.cloudwatch_alarms[each.key], "evaluation_periods", null)
+  metric_name         = lookup(local.cloudwatch_alarms[each.key], "metric_name", null)
+  metric_query        = lookup(local.cloudwatch_alarms[each.key], "metric_query", [])
+  namespace           = lookup(local.cloudwatch_alarms[each.key], "namespace", null)
+  period              = lookup(local.cloudwatch_alarms[each.key], "period", null)
+  statistic           = lookup(local.cloudwatch_alarms[each.key], "statistic", null)
+  threshold           = lookup(local.cloudwatch_alarms[each.key], "threshold", null)
+  dimensions          = lookup(local.cloudwatch_alarms[each.key], "dimensions", null)
+
+  alarm_actions = lookup(local.cloudwatch_alarms[each.key], "alarm_actions", var.default_alarm_actions)
+
+  tags = local.tags
+}
